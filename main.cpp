833e271829533abfec6423dec2216716a25d1b1e
Keichi Takahashi <hello@keichi.dev>
2023-04-05 21:08:24 +0900

    In-place partial key-value sort

--- main.cpp
+++ main.cpp
@@ -1,8 +1,11 @@
 // Radix selection using Kokkos; loosely based on PyTorch's implementation
 // https://github.com/pytorch/pytorch/blob/master/caffe2/operators/top_k_radix_selection.cuh
+#include "Kokkos_Pair.hpp"
 #include <Kokkos_Core.hpp>
+#include <Kokkos_NestedSort.hpp>
 #include <Kokkos_Random.hpp>
 #include <iomanip>
+#include <std_algorithms/Kokkos_Swap.hpp>
 
 const unsigned int RADIX_BITS = 8;
 const unsigned int RADIX_SIZE = 1 << RADIX_BITS;
@@ -37,21 +40,21 @@ template <class T> struct reduction_identity<struct find_result<T>> {
 int main(int argc, char *argv[])
 {
     int N = 10;
-    int L = 100000;
-    int K = 10;
+    int L = 10;
+    int K = 3;
 
     Kokkos::ScopeGuard kokkos(argc, argv);
 
     Kokkos::View<float **> distances("distances", N, L);
     Kokkos::View<int **> indices("indices", N, L);
-    Kokkos::View<int **> topk("out", N, K);
 
     Kokkos::Random_XorShift64_Pool<> random_pool(42);
 
     Kokkos::parallel_for(
-        "shuffle", N, KOKKOS_LAMBDA(int i) {
+        "init", N, KOKKOS_LAMBDA(int i) {
             for (int j = 0; j < L; j++) {
                 distances(i, j) = j + 0.1f;
+                indices(i, j) = j;
             }
 
             auto generator = random_pool.get_state();
@@ -59,9 +62,7 @@ int main(int argc, char *argv[])
             // Fisher–Yates shuffle
             for (int j = 0; j < L - 2; j++) {
                 int k = generator.urand(j, L);
-                float tmp = distances(i, j);
-                distances(i, j) = distances(i, k);
-                distances(i, k) = tmp;
+                Kokkos::Experimental::swap(distances(i, j), distances(i, k));
             }
 
             random_pool.free_state(generator);
@@ -70,22 +71,23 @@ int main(int argc, char *argv[])
     typedef Kokkos::View<int *,
                          Kokkos::DefaultExecutionSpace::scratch_memory_space,
                          Kokkos::MemoryUnmanaged>
-        ScratchBins;
+        ScratchIndex;
+
+    int lv0_scratch_size =
+        ScratchIndex::shmem_size(RADIX_SIZE) + ScratchIndex::shmem_size(K);
 
     Kokkos::parallel_for(
         "radix_select",
         Kokkos::TeamPolicy<>(N, Kokkos::AUTO)
-            .set_scratch_size(
-                0, Kokkos::PerTeam(ScratchBins::shmem_size(RADIX_SIZE)))
-            .set_scratch_size(
-                1, Kokkos::PerTeam(ScratchBins::shmem_size(RADIX_SIZE))),
+            .set_scratch_size(0, Kokkos::PerTeam(lv0_scratch_size)),
         KOKKOS_LAMBDA(const Kokkos::TeamPolicy<>::member_type &member) {
             int i = member.league_rank();
             int k = K;
             unsigned int mask = 0, desired = 0;
             bool found = false;
 
-            ScratchBins bins(member.team_scratch(0), RADIX_SIZE);
+            ScratchIndex bins(member.team_scratch(0), RADIX_SIZE);
+            ScratchIndex topk(member.team_scratch(0), K);
 
             for (int digit_pos = 32 - RADIX_BITS; digit_pos >= 0 && !found;
                  digit_pos -= RADIX_BITS) {
@@ -112,15 +114,11 @@ int main(int argc, char *argv[])
                 for (int j = 0; j < RADIX_SIZE; j++) {
                     int count = bins(j);
 
-                    if (count == 1 && k == 1) {
+                    if (count >= k) {
                         mask |= RADIX_MASK << digit_pos;
                         desired |= j << digit_pos;
 
-                        found = true;
-                        break;
-                    } else if (count >= k) {
-                        mask |= RADIX_MASK << digit_pos;
-                        desired |= j << digit_pos;
+                        if (count == 1) found = true;
 
                         break;
                     }
@@ -146,31 +144,42 @@ int main(int argc, char *argv[])
                                   [=](int j, int &partial_sum, bool is_final) {
                                       if (distances(i, j) <= res.val) {
                                           if (is_final && partial_sum < K) {
-                                              topk(i, partial_sum) = j;
+                                              topk(partial_sum) = j;
                                           }
                                           partial_sum++;
                                       }
                                   });
-        });
 
-    const auto topk_mirror = Kokkos::create_mirror_view_and_copy(
-        Kokkos::DefaultHostExecutionSpace(), topk);
+            Kokkos::parallel_for(
+                Kokkos::TeamThreadRange(member, K), [=](int j) {
+                    Kokkos::Experimental::swap(distances(i, topk(j)),
+                                               distances(i, j));
+                    Kokkos::Experimental::swap(indices(i, topk(j)),
+                                               indices(i, j));
+                });
+
+            Kokkos::Experimental::sort_by_key_team(
+                member, Kokkos::subview(distances, i, Kokkos::make_pair(0, K)),
+                Kokkos::subview(indices, i, Kokkos::make_pair(0, K)));
+        });
 
     const auto distances_mirror = Kokkos::create_mirror_view_and_copy(
         Kokkos::DefaultHostExecutionSpace(), distances);
+    const auto indices_mirror = Kokkos::create_mirror_view_and_copy(
+        Kokkos::DefaultHostExecutionSpace(), indices);
 
-    std::cout << "top-" << K << " indices:" << std::endl;
-    for (int i = 0; i < N; i++) {
-        for (int j = 0; j < K; j++) {
-            std::cout << topk_mirror(i, j) << ", ";
-        }
-        std::cout << std::endl;
-    }
     std::cout << std::endl;
     std::cout << "top-" << K << " distances:" << std::endl;
     for (int i = 0; i < N; i++) {
-        for (int j = 0; j < K; j++) {
-            std::cout << distances_mirror(i, topk_mirror(i, j)) << ", ";
+        for (int j = 0; j < L; j++) {
+            std::cout << distances_mirror(i, j) << ", ";
+        }
+        std::cout << std::endl;
+    }
+    std::cout << "top-" << K << " indices:" << std::endl;
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < L; j++) {
+            std::cout << indices_mirror(i, j) << ", ";
         }
         std::cout << std::endl;
     }
